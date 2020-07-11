8cbcb003ecc5f77d388bdf72a1701056b7366fc9
Илья Тербушев <terbushev.ide@students.dvfu.ru>
2020-07-11 18:47:21 +1000

    1

---
+++ курсач/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace курсач
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Provider_name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Provider_name
+            // 
+            this.Provider_name.AutoSize = true;
+            this.Provider_name.Location = new System.Drawing.Point(177, 9);
+            this.Provider_name.Name = "Provider_name";
+            this.Provider_name.Size = new System.Drawing.Size(63, 13);
+            this.Provider_name.TabIndex = 0;
+            this.Provider_name.Text = "Провайдер";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 262);
+            this.Controls.Add(this.Provider_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Provider_name;
+    }
+}
