b80ec06169bd151d56618b39fa2f16e75973709f
Илья Тербушев <terbushev.ide@students.dvfu.ru>
2020-07-12 19:10:31 +1000

    Сделал работу с файлом, но пока только с провайдером/тарифом

--- курсач/Form2.cs
+++ курсач/Form2.cs
@@ -396,19 +396,68 @@ namespace курсач
                // file.Write("\n");
 
             }
+            file.WriteLine("//");
             output(file, pr.left);
             output(file, pr.right);
         }
         private void to_file_Click(object sender, EventArgs e)
         {
             StreamWriter file_out = new StreamWriter(@"a:\gitjub\курсач\output.txt");
-            file_out.WriteLine("РАБОТАЙ");
+            //file_out.WriteLine("РАБОТАЙ");
             output(file_out, provider.main);
             file_out.Close();
         }
 
+        public void input(StreamReader file)
+        {
+            String temp;
+            string provider_name;
+            string helper = "";
+            string[] info = new string[4];
+            int j = 0;
+            tree_providers.root pr;
+            while (file.Peek() > -1)
+            {
+                temp = file.ReadLine();
+                provider_name = temp;
+                provider.add_Provider(provider_name);
+                pr = provider.find(provider_name);
+                
+                while (true)
+                {
+                    temp = file.ReadLine();
+                    helper = "";
+                    if (temp == "//")
+                        break;
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        if (temp[i] == '/')
+                        {
+                            i++;
+                            info[j] = helper;
+                            j++;
+                            helper = "";
+                        }
+                        helper += temp[i];
+                    }
+                    j = 0;
+                    info[3] = helper;
+                    if ((!check_for_int(info[1])) && (!check_for_int(info[2])) && (!check_for_int(info[3])))
+                    {
+                        error_number = 2;
+                        message_box(error_number);
+                    }
+                    provider.add_tariph(info[0], Convert.ToInt32(info[1]), provider_name);
+
+                    tariph.add(tariph.getkey(info[0]), info[0], Convert.ToInt32(info[2]), Convert.ToInt32(info[3]), pr);
+                }
+            }
+        }
         private void from_file_button_Click(object sender, EventArgs e)
         {
+            StreamReader file_in = new StreamReader(@"a:\gitjub\курсач\output.txt");
+            input(file_in);
+            file_in.Close();
 
         }
     }
